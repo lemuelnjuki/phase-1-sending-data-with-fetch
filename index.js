7f70fc587de592995d1297287b84f80695c3c031
lemuelnjuki <lemuel.njuki@student.moringaschool.com>
2022-08-25 17:24:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+//Add your code here
+function submitData(username, emailaddress) {
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json",
+        "Accept" : "application/json"
+      },
+      body: JSON.stringify({
+        name: username,
+        email : emailaddress
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector("body").append(data.id)
+    })
+    .catch(error => {
+        alert("SINA MAONI");
+        console.log(error.message);
+        document.querySelector("body").append(error.message)
+    })
+  }
+
+
